42a5a22d29d7a99245b758b2ce5198b9ddb81cfa
seanshi <496458172@qq.com>
2017-08-24 21:23:40 +0800

    kafka update

--- src/main/scala/com/spark/bigdata/kafka/KafkaConsumeMS.java
+++ src/main/scala/com/spark/bigdata/kafka/KafkaConsumeMS.java
@@ -1,6 +1,40 @@
 package com.spark.bigdata.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class KafkaConsumeMS {
+
+    private static KafkaConsumer<String, String> kafkaConsumer=KafkaUtil.getConsumer();
+    final static  int minBatchSize = 200;
+    public static void KafaConuseMS(List<String> topics){
+
+        kafkaConsumer.subscribe(topics);
+        kafkaConsumer.seekToBeginning(new ArrayList<TopicPartition>());
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<ConsumerRecord<String, String>>();
+        while (true) {
+            //超时
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.topic());
+                buffer.add(record);
+            }
+            /**
+             *
+             */
+            if (buffer.size() >= minBatchSize) {
+
+                kafkaConsumer.commitSync();
+                buffer.clear();
+            }
+        }
+    }
 
 
 }
